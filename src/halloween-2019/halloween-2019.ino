a47a1350b2990a68cb1a5d4561e74eb6b0a9bf37
Alexandro D. Almeida <alexandro@tegra.com.br>
2019-10-23 22:13:28 -0300

    HALLOWEEN THEME

--- src/halloween-2019/halloween-2019.ino
+++ src/halloween-2019/halloween-2019.ino
@@ -1,18 +1,70 @@
 #include <TaskScheduler.h>
 #include <Chrono.h>
+#include "pitches.h"
 
 
 void scareLoop(void);
+void thereminLoop(void);
 
 Scheduler runner;
 Task scareTask(2, TASK_FOREVER, &scareLoop);
+Task thereminTask(12, TASK_FOREVER, &thereminLoop);
 
+boolean thereminOn = true;
+
+void thereminLoop(void) {
+  static Chrono _noteChrono;
+  static unsigned int melody[] = {NOTE_CS6,NOTE_FS5,NOTE_FS5,NOTE_CS6,NOTE_FS5,NOTE_FS5,NOTE_CS6,NOTE_D5,NOTE_FS5,
+               NOTE_CS6,NOTE_FS5,NOTE_FS5,NOTE_CS6,NOTE_FS5,NOTE_FS5,NOTE_CS6,NOTE_D5,NOTE_FS5,
+               NOTE_C6,NOTE_F5,NOTE_F5,NOTE_C6,NOTE_F5,NOTE_F5,NOTE_C6,NOTE_F5,NOTE_CS5,NOTE_F5,
+               NOTE_C6,NOTE_F5,NOTE_F5,NOTE_C6,NOTE_F5,NOTE_F5,NOTE_C6,NOTE_F5,NOTE_CS5,NOTE_F5,
+               NOTE_B5,NOTE_E5,NOTE_E5,NOTE_B5,NOTE_E5,NOTE_E5,NOTE_B5,NOTE_E5,NOTE_C6,NOTE_E5,
+               NOTE_B5,NOTE_E5,NOTE_E5,NOTE_B5,NOTE_E5,NOTE_E5,NOTE_B5,NOTE_E5,NOTE_C6,NOTE_E5};
+  static unsigned short noteDurations[] = {4,4,4,4,4,4,4,4,4,
+                      4,4,4,4,4,4,4,4,4,
+                      4,4,4,4,4,4,4,4,4,4,
+                      4,4,4,4,4,4,4,4,4,4,
+                      4,4,4,4,4,4,4,4,4,4,
+                      4,4,4,4,4,4,4,4,4,4};
+  static unsigned short note = 0;
+
+  static unsigned int vibrato[] = {0,4,8,11,13,15,15,15,13,11,8,4};
+  static unsigned short vibratoLen = 12;
+  static unsigned short x = 0;
+
+  static int noteDuration = 0;
+  static boolean playNote = true;
+
+  if (thereminOn && playNote && melody[note]) {
+    //tone(4, melody[note]);
+    tone(4, melody[note]+vibrato[x++]*(melody[note]/100));
+    if (x == 12) {
+      x = 0;
+    }
+  }
+
+  if (_noteChrono.hasPassed(noteDuration)) {
+    _noteChrono.restart();
+    if (playNote) {
+      noTone(4);
+      playNote = false;
+      noteDuration *= 0.05;
+    } else {
+      note++;
+      if (note == 58) {
+        note = 0;
+      }
+      noteDuration = 1200 / noteDurations[note];
+      playNote = true;
+      x = 0;
+    }
+  } 
+}
 
 void scareLoop()  {
   static Chrono _lightChrono;
   static Chrono _waitChrono;
   
-
   static unsigned short _state = 0; //Estado da Task
   static unsigned long  _wtime = 0; //Tempo de espera
   static unsigned long  _ltime = 0; //Tempo de flash da luz
@@ -30,6 +82,9 @@ void scareLoop()  {
         _lightChrono.restart();
         _ltime = 0;
         _state++;
+        thereminOn=false;
+        noTone(4);
+        
       }
       break;
     case 2:
@@ -49,6 +104,7 @@ void scareLoop()  {
         _state = 0;
         noTone(2);
         digitalWrite(3, HIGH);
+        thereminOn=true;
       }
       
       break;
@@ -57,26 +113,11 @@ void scareLoop()  {
     break;
      
   }
-  /*
-  unsigned int blinkTime = random(50,100);
-  digitalWrite(3, LOW);
-  while(!noiseChrono.hasPassed(ntime))  {  // change "500" for different durations in ms.
-    tone(2, random(300, 1300));   // change the parameters of random() for different sound
-    if(lightChrono.hasPassed(blinkTime)) {
-      lightChrono.restart();
-      blinkTime = random(50,100);
-      digitalWrite(3, !digitalRead(3));
-    }
-  }
-  noTone(2);
-  digitalWrite(3, HIGH);
-  */
 }
-
 
 void setup() {
   randomSeed(analogRead(0));
-  //runner.init();
+
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   digitalWrite(3, HIGH);
@@ -84,6 +125,8 @@ void setup() {
   runner.init();
   runner.addTask(scareTask);
   scareTask.enable();
+  runner.addTask(thereminTask);
+  thereminTask.enable();
   
 }
 
